f99469f0d8af3c7403668b3ff54ee8e2ef8c0f5c
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-07 13:28:16 -0800

    import and render pages with react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import LandingPage from "./pages/LandingPage";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
+import WlecomPage from "./pages/WlecomPage";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<LandingPage />} />
+          <Route path="signin" element={<SigninPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="welcome" element={<WlecomPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
