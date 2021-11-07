298f288d7660d3ffa1797eb48d7859e9df3160e2
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-06 23:25:12 -0700

    create links and implement logout feature

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import currentUserQuery from "../queries/currentUser";
+import logoutMutation from "../mutation/logout";
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-}
+  const { data } = useQuery(currentUserQuery);
+  const [logout] = useMutation(logoutMutation);
 
-export default Header
+  const logoutHandler = () => {
+    logout({
+      refetchQueries: [{ query: currentUserQuery }]
+    });
+  };
+
+  const renderButtons = () => {
+    if (data?.user) {
+      return (
+        <li>
+          <a href onClick={logoutHandler}>
+            logout
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo left">
+          Home
+        </Link>
+        <ul className="right">{renderButtons()}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
