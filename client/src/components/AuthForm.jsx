3e6e24351b19af1ad8814e0b8036017e6b8f4d61
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-11-07 13:31:43 -0800

    form for user to login and signup

---
+++ client/src/components/AuthForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const INITIAL_STATE = { email: "", password: "" };
+
+const AuthForm = ({ onLogin, onSignup, errors }) => {
+  const [input, setInput] = useState(INITIAL_STATE);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    onLogin ? onLogin(input) : onSignup(input);
+    setInput(INITIAL_STATE);
+  };
+
+  return (
+    <div className="row">
+      <form className="col s6" onSubmit={submitHandler}>
+        <div className="input-field">
+          <input
+            placeholder="Email"
+            type="email"
+            value={input.email}
+            onChange={(e) => setInput({ ...input, email: e.target.value })}
+          />
+        </div>
+        <div className="input-field">
+          <input
+            placeholder="Password"
+            type="password"
+            value={input.password}
+            onChange={(e) => setInput({ ...input, password: e.target.value })}
+          />
+        </div>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+        {errors.map((err, index) => (
+          <p key={index} className="error">
+            {err}
+          </p>
+        ))}
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
